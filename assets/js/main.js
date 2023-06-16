acd43676870eec9110a8b541777cab4aee5af2dc
thiago alves <thiago87gsa@gmail.com>
2023-06-16 10:40:18 -0300

    fazendo alguns ajustes importantes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,51 @@
-let iniciar = document.querySelector('.iniciar')
-let pausar = document.querySelector('.pausar')
-let zerar = document.querySelector('.zerar')
-let relogio = document.querySelector('.relogio')
+function cronometro() {
+    let iniciar = document.querySelector('.iniciar')
+    let relogio = document.querySelector('.relogio')
 
-let seg = 0
-let min = 0
-let hor = 0
-iniciar.addEventListener('click',(e)=>{
-    relogio.style.color = 'black'
-    let cronometro = setInterval(function(){
-        seg++
-        if(seg === 60){
-            min++
+    let seg = 0
+    let min = 0
+    let hor = 0
+    let cronometro
+
+    function inicarRelogio() {
+        relogio.classList.remove('pausado')
+        cronometro = setInterval(function () {
+            seg++
+            if (seg === 60) {
+                min++
+                seg = 0
+            }
+            if (min === 60) {
+                hor++
+                min = 0
+            }
+            relogio.innerHTML = `${(hor.toString().padStart(2, '0'))}:${(min).toString().padStart(2, '0')}:${(seg).toString().padStart(2, '0')}`
+        }, 1000)
+    }
+
+    document.addEventListener('click', function (e) {
+        let el = e.target
+
+        if (el.classList.contains('iniciar')) {
+            iniciar.disabled = true
+            inicarRelogio()
+        }
+        if (el.classList.contains('pausar')) {
+            iniciar.disabled = false
+            relogio.classList.add('pausado')
+            clearInterval(cronometro)
+        }
+        if (el.classList.contains('zerar')) {
+            iniciar.disabled = false
+            relogio.classList.remove('pausado')
+            clearInterval(cronometro)
+            relogio.innerHTML = '00:00:00'
             seg = 0
-        }
-        if(min === 60){
-            hor++
             min = 0
+            hor = 0
         }
-        relogio.innerHTML = `${(hor.toString().padStart(2, '0'))}:${(min).toString().padStart(2,'0')}:${(seg).toString().padStart(2,'0')}`
-    }, 1000)
-
-    pausar.addEventListener('click',(e)=>{
-        relogio.style.color = 'red'
-        clearInterval(cronometro)
     })
+}
+cronometro()
 
-    zerar.addEventListener('click',(e)=>{
-        relogio.style.color = 'black'
-        clearInterval(cronometro)
-        relogio.innerHTML = '00:00:00'
-        seg = 0
-        min = 0
-        hor = 0
-    })
-})
+
